65561a513801c0dfae5ac4bfd1ce30fd808ce52d
Shine༒︎🌙 <domenicohalo1101@hotmail.com>
2024-10-08 05:58:46 +0200

    Update gp-setbye.js

--- plugins/gp-setbye.js
+++ plugins/gp-setbye.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
 if (text) {
 global.db.data.chats[m.chat].sBye = text
-m.reply('ok')
+m.reply('fatto')
 } else throw `inserisci il messaggio`
 }
 handler.help = ['setbye <text>']
